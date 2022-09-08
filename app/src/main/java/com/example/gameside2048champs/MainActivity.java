305aaee35907fb9bc9c4a3d03291a52a395ddfc4
DevStar999 <wolverinerage99@gmail.com>
2022-09-08 23:41:33 +0530

    Wired up the MainActivity to the PreGameFragment

--- app/src/main/java/com/example/gameside2048champs/MainActivity.java
+++ app/src/main/java/com/example/gameside2048champs/MainActivity.java
@@ -1,14 +1,56 @@
 package com.example.gameside2048champs;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.gameside2048champs.dialogs.ArrivingFeatureDialog;
+
+public class MainActivity extends AppCompatActivity implements PreGameFragment.OnPreGameFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AppCompatButton startPreGameFragmentButton = findViewById(R.id.start_game_pregame_fragment_button);
+        startPreGameFragmentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PreGameFragment fragment = new PreGameFragment();
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction transaction = fragmentManager.beginTransaction();
+                transaction.setCustomAnimations(R.anim.enter_from_right, R.anim.exit_to_right,
+                        R.anim.enter_from_right, R.anim.exit_to_right);
+                transaction.addToBackStack(null);
+                transaction.replace(R.id.full_screen_fragment_container, fragment, "PREGAME_FRAGMENT")
+                        .commit();
+            }
+        });
+    }
+
+    @Override
+    public void onPreGameFragmentInteractionBackClicked() {
+        onBackPressed();
+    }
+
+    @Override
+    public void onPreGameFragmentInteractionStartGame(String gameMode, int gameMatrixColumns, int gameMatrixRows) {
+        Intent intent = new Intent(MainActivity.this, GameActivity.class);
+        intent.putExtra("gameMode", gameMode);
+        intent.putExtra("gameMatrixColumns", gameMatrixColumns);
+        intent.putExtra("gameMatrixRows", gameMatrixRows);
+        startActivity(intent);
+        finish();
+    }
+
+    @Override
+    public void onPreGameFragmentInteractionShowArrivingFeatureDialog() {
+        new ArrivingFeatureDialog(this).show();
     }
 }
