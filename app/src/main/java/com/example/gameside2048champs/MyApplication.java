22c5ca84b944a6b3b15325e2bccfa4bef98d343d
DevStar999 <wolverinerage99@gmail.com>
2023-03-19 00:28:01 +0530

    Completed the setup of AdMob into the project

--- app/src/main/java/com/example/gameside2048champs/MyApplication.java
+++ app/src/main/java/com/example/gameside2048champs/MyApplication.java
@@ -1,7 +1,16 @@
 package com.example.gameside2048champs;
 
+import static com.google.android.gms.ads.RequestConfiguration.MAX_AD_CONTENT_RATING_G;
+import static com.google.android.gms.ads.RequestConfiguration.TAG_FOR_CHILD_DIRECTED_TREATMENT_TRUE;
+
 import android.app.Application;
 
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.RequestConfiguration;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 import com.google.android.gms.games.PlayGamesSdk;
 
 public class MyApplication extends Application {
@@ -10,5 +19,16 @@ public class MyApplication extends Application {
     public void onCreate() {
         super.onCreate();
         PlayGamesSdk.initialize(this);
+
+        // Initialising AdMob
+        RequestConfiguration requestConfiguration = MobileAds.getRequestConfiguration().toBuilder()
+                .setTagForChildDirectedTreatment(TAG_FOR_CHILD_DIRECTED_TREATMENT_TRUE)
+                .setMaxAdContentRating(MAX_AD_CONTENT_RATING_G)
+                .build();
+        MobileAds.setRequestConfiguration(requestConfiguration);
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(@NonNull InitializationStatus initializationStatus) {}
+        });
     }
 }
